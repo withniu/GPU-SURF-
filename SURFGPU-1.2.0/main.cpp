619977a9c2e98fac421e4f6549edaa7faabcb257
Yedong Niu <niuyedong@gmail.com>
2012-04-01 01:36:42 -0400

    test high res

--- SURFGPU-1.2.0/main.cpp
+++ SURFGPU-1.2.0/main.cpp
@@ -317,7 +317,7 @@ int mainStaticMatch()
 	cvSaveImage("result2_gpu.jpg",img2);
 
 	IplImage *img = cvCreateImage(cvSize(img1->width + img2->width,
-										 img1->height),IPL_DEPTH_8U, 1 ); 
+										 img1->height),img1->depth, 1 ); 
 	cvZero( img );	
 	cvSetImageROI( img, cvRect( 0, 0, img1->width, img1->height ) ); 
     cvCopy(img1, img);
