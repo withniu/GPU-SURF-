8e87ff1d0786d2c90bffeffdfec3781c9b8beeec
Yedong Niu <niuyedong@gmail.com>
2012-04-01 03:26:59 -0400

    test high res

--- OpenSURFcpp/src/main.cpp
+++ OpenSURFcpp/src/main.cpp
@@ -242,7 +242,11 @@ int mainStaticMatch()
   surfDetDes(img1,ipts1,false,4,4,2,0.001f);
   surfDetDes(img2,ipts2,false,4,4,2,0.001f);
 
+  std::cout << "im1" << std::endl;
+  std::cout << "Size:" << ipts1.size() << std::endl;
 
+  std::cout << "im2" << std::endl;
+  std::cout << "Size:" << ipts2.size() << std::endl;
 
   
   end2 = clock();
